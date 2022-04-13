fedb548b698133104b8ae886046d09ea4e91ec9b
Mhdtarek <mhdtarekalkasemi@gmail.com>
2022-04-13 21:25:45 +0200

    adderade fix

--- js/init.js
+++ js/init.js
@@ -5,3 +5,20 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+$(".dropdown-trigger").dropdown();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
